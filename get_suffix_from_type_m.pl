0dfec89920d4ab69ec37c496f7d3560960eecd8e
robbie-hatley <Robbie.Hatley@gmail.com>
2023-08-12 20:29:35 -0700

    m

---
+++ get_suffix_from_type_m.pl
@@ -0,0 +1,53 @@
+sub get_suffix_from_type :prototype($) ($type) {
+   !defined $type and return '.unk';       # If $type is undefined, use extension ".unk".
+   $type = lc $type;                       # Lower-case the type.
+   $type =~ s%/x(-|.)%/%;                  # Get rid of "unregistered "markers ("x-" and variants).
+   $type =~ s%\+\pL+$%%;                   # Get rid of alternate type interpretations (eg, xml for svg).
+   for ($type) {                           # Match normalized type against known types and choose extension:
+      m%^video/msvideo$%                                   and return '.avi'  ;
+      m%^image/bmp$%                                       and return '.bmp'  ;
+      m%^application/freearc$%                             and return '.arc'  ;
+      m%^text/css$%                                        and return '.css'  ;
+      m%^text/csv$%                                        and return '.csv'  ;
+      m%^application/msword$%                              and return '.doc'  ;
+      m%^application/epub+zip$%                            and return '.epub' ;
+      m%^image/gif$%                                       and return '.gif'  ;
+      m%^text/html$%                                       and return '.html' ;
+      m%^image/vnd.microsoft.icon$%                        and return '.ico'  ;
+      m%^application/java-archive$%                        and return '.jar'  ;
+      m%^image/jpeg$%                                      and return '.jpg'  ;
+      m%^text/javascript$%                                 and return '.js'   ;
+      m%^application/json$%                                and return '.json' ;
+      m%^audio/midi$%                                      and return '.mid'  ;
+      m%^audio/mpeg$%                                      and return '.mp3'  ;
+      m%^video/mpeg$%                                      and return '.mpg'  ;
+      m%^video/mp4$%                                       and return '.mp4'  ;
+      m%^application/vnd.oasis.opendocument.presentation$% and return '.odp'  ;
+      m%^application/vnd.oasis.opendocument.spreadsheet$%  and return '.ods'  ;
+      m%^application/vnd.oasis.opendocument.text$%         and return '.odt'  ;
+      m%^audio/ogg$%                                       and return '.ogg'  ;
+      m%^font/otf$%                                        and return '.otf'  ;
+      m%^image/png$%                                       and return '.png'  ;
+      m%^application/pdf$%                                 and return '.pdf'  ;
+      m%^application/httpd-php$%                           and return '.php'  ;
+      m%^application/vnd.ms-powerpoint$%                   and return '.ppt'  ;
+      m%^application/vnd.rar$%                             and return '.rar'  ;
+      m%^application/rtf$%                                 and return '.rtf'  ;
+      m%^application/sh$%                                  and return '.sh'   ;
+      m%^image/svg+xml$%                                   and return '.svg'  ;
+      m%^application/tar$%                                 and return '.tar'  ;
+      m%^image/tiff$%                                      and return '.tiff' ;
+      m%^font/ttf$%                                        and return '.ttf'  ;
+      m%^text/plain$%                                      and return '.txt'  ;
+      m%^audio/wav$%                                       and return '.wav'  ;
+      m%^audio/webm$%                                      and return '.weba' ;
+      m%^video/webm$%                                      and return '.webm' ;
+      m%^image/webp$%                                      and return '.webp' ;
+      m%^application/vnd.ms-excel$%                        and return '.xls'  ;
+      m%^text/xml$%                                        and return '.xml'  ;
+      m%^application/vnd.mozilla.xul+xml$%                 and return '.xul'  ;
+      m%^application/zip$%                                 and return '.zip'  ;
+      m%^application/7z-compressed$%                       and return '.7z'   ;
+   }
+   return '.unk'; # $type is unknown
+} # end sub get_suffix_from_type ($)
