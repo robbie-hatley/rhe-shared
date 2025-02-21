e2bd4eb352f3364dd86ffa8afd3b6b0f9bdf94a3
robbie-hatley <Robbie.Hatley@gmail.com>
2025-02-20 16:04:01 -0800

    huw-test update

--- src/huw-test.c
+++ src/huw-test.c
@@ -17,9 +17,10 @@ void ADCStats() {
    int i = 0;
    unsigned char *p;
    p=(unsigned char *)&adcISRStack[0];
-   printf("0th byte of 0th cell of adcISRStack = %x\n", *p);
-   while (*p++ == (unsigned char)0xa5) { /* comparing ONE byte!!!   */
-      printf("i = %u here.\n", ++i);     /* announce each new i     */
+   printf("0th byte of 0th cell of adcISRStack = 0x%x\n", *p);
+   while (*p == (unsigned char)0xa5) {                  /* compare ONE byte!!!         */
+      printf("i = %u here and *p = 0x%x.\n", ++i, *p);  /* announce each new i and *p  */
+      ++p;                                              /* advance pointer ONE byte!!! */
    }
    printf("Final value of i = %u\n", i);
    printf("ISR stack high water = %u\n", i/sizeof(unsigned int));
